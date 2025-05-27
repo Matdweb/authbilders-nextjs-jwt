a474b57a8422e511ce2ad84f826443346212cf58
mat_dweb <mat.dweb28@gmail.com>
2025-05-26 19:41:55 -0600

    fix(jwt): remove any types

--- src/app/lib/utils/jwt.ts
+++ src/app/lib/utils/jwt.ts
@@ -1,9 +1,10 @@
 import { SignJWT, jwtVerify } from "jose";
+import type { Session } from "../defintions";
 
 const secretKey = process.env.JWT_SECRET_KEY || "";
 const key = new TextEncoder().encode(secretKey);
 
-export async function encrypt(payload: any) {
+export async function encrypt(payload: Session): Promise<string> {
     return await new SignJWT(payload)
         .setProtectedHeader({ alg: "HS256" })
         .setIssuedAt()
@@ -11,7 +12,7 @@ export async function encrypt(payload: any) {
         .sign(key);
 }
 
-export async function decrypt(input: string): Promise<any> {
+export async function decrypt(input: string): Promise<Session | null> {
     try {
         const { payload } = await jwtVerify(input, key, {
             algorithms: ["HS256"],
