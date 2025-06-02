9099e8a4add68fe28e904fdcff465c5f373e53ee
mat_dweb <mat.dweb28@gmail.com>
2025-06-02 14:21:20 -0600

    feat: add email verifiaction to UserInfo field

--- src/components/(AuthBilders)/UserInfo.tsx
+++ src/components/(AuthBilders)/UserInfo.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { useState } from "react";
-import { Tooltip, User, Chip, Code } from "@heroui/react";
+import { Tooltip, User, Chip, Code, Spinner } from "@heroui/react";
 import { usePathname } from "next/navigation";
 import type { User as UserType } from "@/app/lib/(AuthBilders)/defintions";
 import { CloseIcon } from "./icons";
+import { sendEmailVerification } from "@/app/lib/(AuthBilders)/utils/email";
+import { sendEmailVerificationToast } from "./Alerts/Toasts";
 
 type UserInfoProps = {
   data: UserType | undefined | null;
@@ -12,12 +14,27 @@ type UserInfoProps = {
 export function UserInfo({ data }: UserInfoProps) {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isSending, setIsSending] = useState<boolean>(false);
 
   if (["/login", "/signUp"].includes(pathname)) return null;
 
   if (!data) return null;
 
   const { email, name, picture, email_verified } = data;
+
+  const handleSendEmailVerification = async () => {
+    if (email_verified) return;
+    setIsSending(true);
+
+    const response = await sendEmailVerification(email || "");
+
+    sendEmailVerificationToast({
+      title: response.success ? "Email verification sent" : "Failed to send verification email",
+      description: (response.success ? "Check your inbox and junk" : "Try again"),
+      color: response.success ? "success" : "danger",
+    });
+    setIsSending(false);
+  };
 
   function SessionDisplay() {
     return (
@@ -40,8 +57,14 @@ export function UserInfo({ data }: UserInfoProps) {
           color={email_verified ? "success" : "danger"}
           variant="dot"
           className="mt-2 border-none h-auto cursor-pointer py-1 bg-white/10 rounded-lg min-w-full"
+          onClick={handleSendEmailVerification}
         >
-          {email_verified ? "Email verified" : "Email not verified"}
+          {
+            isSending ?
+              <Spinner color="white" variant="dots" className="max-h-1" />
+              :
+              (email_verified ? "Email verified" : "Email not verified")
+          }
         </Chip>
       </div >
     )
